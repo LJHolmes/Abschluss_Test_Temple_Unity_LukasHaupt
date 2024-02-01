e694299dc482c41532799e6f5afc2a83c84c8a12
LJHolmes <lukashaupt@gmx.net>
2024-02-01 15:56:40 +0100

    Prepare RizzleThree

--- Abschluss_Test_Temple/Assets/Scripts/Rizzle3Master.cs
+++ Abschluss_Test_Temple/Assets/Scripts/Rizzle3Master.cs
@@ -5,6 +5,11 @@ public class Rizzle3Master : MonoBehaviour
 {
     private SoundManager soundManager;
 
+    [SerializeField] private GameObject winScreenPanel;
+
+    [SerializeField] private int correctStatueCount = 0;
+    [SerializeField] private int correctStatueMax = 4;
+
     void Start()
     {
         soundManager = GameObject.Find("Main Camera").GetComponent<SoundManager>();
@@ -17,7 +22,10 @@ public class Rizzle3Master : MonoBehaviour
 
     private void CheckLevelIsWon()
     {
-
+        if (correctStatueCount >= correctStatueMax)
+        {
+            winScreenPanel.SetActive(true);
+        }
     }
 
     private void ChangeScene(int sceneIndex)
